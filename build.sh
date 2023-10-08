0a30dce7b8e2b38cd151b9ec1e942743e49a762e
WakelessSloth56 <wsloth56@foxmail.com>
2023-10-08 17:13:21 +0800

    ci(pages): minify html and js

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ mkdir public/lib
 mkdir src/lib
 
 npm install -g typescript
+npm install -g terser
+npm install -g html-minifier-terser
 
 curl -L https://github.com/jabbany/CommentCoreLibrary/raw/19db2962ed0ce637a2b99facdf8634d51bb1b503/dist/CommentCoreLibrary.js >src/lib/CommentCoreLibrary.js
 curl -L https://github.com/jabbany/CommentCoreLibrary/raw/19db2962ed0ce637a2b99facdf8634d51bb1b503/dist/css/style.min.css >public/lib/CommentCoreLibrary.css
 
 tsc
+
+echo '{"compress":{"pure_funcs":["console.debug"]},"ecma":2017}' >terser.json
+
+html-minifier-terser --minify-js --collapse-whitespace --remove-comments public/index.html >public/index.html
+terser --config-file terser.json public/player.js >public/player.js
