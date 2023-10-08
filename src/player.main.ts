890288f1ea08964b0d0e52cea254497b5ef9466f
WakelessSloth56 <wsloth56@foxmail.com>
2023-10-08 16:35:05 +0800

    feat(player): copy old project files
    Co-authored-by: lainio24 <lainio24@outlook.com>

---
+++ src/player.main.ts
@@ -0,0 +1,204 @@
+interface PlayerOptions extends StrAnyKV {
+    autoPlay?: boolean;
+    muted?: boolean;
+    fullscreen?: boolean;
+}
+
+class Player {
+    options: PlayerOptions;
+    private readonly metadata: PlayerMetadata;
+    readonly title: string;
+    readonly videoUrl: string;
+    readonly video: HTMLVideoElement;
+    overHour: boolean;
+    readonly style: HTMLStyleElement;
+    readonly container: HTMLDivElement;
+    readonly danmakuUrl: string;
+    commentManager;
+    constructed: boolean;
+    elements: StrGenKV<HTMLElement> = {};
+    _dyn: StrAnyKV = {};
+
+    constructor(
+        container: HTMLDivElement,
+        metadata: PlayerMetadata,
+        title: string,
+        videoUrl: string,
+        danmakuUrl: string,
+        options: PlayerOptions
+    ) {
+        this.metadata = metadata;
+        this.options = options;
+        container.classList.add('player');
+        container.tabIndex = 10;
+        {
+            this.title = title;
+            this.videoUrl = videoUrl;
+            const video = document.createElement('video');
+            container.appendChild(video);
+            video.src = videoUrl;
+            this.video = video;
+            this.overHour = false;
+        }
+        {
+            const style = document.createElement('style');
+            this.style = style;
+            container.appendChild(style);
+        }
+        this.container = container;
+        this.danmakuUrl = danmakuUrl;
+        if (0) this.commentManager = new CommentManager(); // for type intellisense
+        this.__bindElements();
+        this.__bindEvents();
+        {
+            this.options.autoPlay ? this.play() : this.pause();
+            this.options.muted ? this.mute() : this.unmute();
+            this.options.fullscreen
+                ? this.requestFullscreen()
+                : this.setContainerData('fullscreen', false);
+        }
+        this.constructed = true;
+        if (this.options.autoPlay) this.toast('Autoplay');
+        this.focus();
+    }
+    private __bindElements() {
+        for (const data of this.metadata.elements) {
+            appendChild(this.container, data.create(this));
+        }
+    }
+    private __bindEvents() {
+        this.onVideoEvent('loadedmetadata', () => {
+            this.setContainerData('paused', this.video.paused);
+            this.overHour = this.video.duration >= 60 * 60;
+        });
+        this.onVideoEvent('play', () =>
+            this.setContainerData('paused', this.video.paused)
+        );
+        this.onVideoEvent('pause', () =>
+            this.setContainerData('paused', this.video.paused)
+        );
+        this.onVideoEvent('volumechange', () =>
+            this.setContainerData('muted', this.video.muted)
+        );
+        this.onPlayerEvent('fullscreenchange', () => {
+            const fullscreen = document.fullscreenElement === this.container;
+            this.setContainerData('fullscreen', fullscreen ? true : false);
+            this.firePlayerEvent(fullscreen ? 'fullscreen' : 'fullscreenexit');
+        });
+        {
+            new ResizeObserver(() => {
+                this.video.dispatchEvent(
+                    new CustomEvent('resize', {
+                        detail: {
+                            width: this.video.offsetWidth,
+                            height: this.video.offsetHeight,
+                        },
+                    })
+                );
+            }).observe(this.video);
+        }
+        bindMetadataEvents(this.metadata, {
+            playerEvent: [this.container, () => [this]],
+            videoEvent: [this.video, () => [this, this.video]],
+        });
+    }
+    focus() {
+        this.container.focus();
+    }
+    setContainerData(key: string, value: any) {
+        this.container.dataset[key] = value;
+    }
+    getContainerData(key: string) {
+        return this.container.dataset[key];
+    }
+    onVideoEvent(type: string, listener: EventListenerOrEventListenerObject) {
+        this.video.addEventListener(type, listener);
+    }
+    onPlayerEvent(type: string, listener: EventListenerOrEventListenerObject) {
+        this.container.addEventListener(type, listener);
+    }
+    firePlayerEvent(type: string, detail?: any) {
+        this.container.dispatchEvent(
+            new CustomEvent(type, detail ? { detail: detail } : null)
+        );
+    }
+    toast(html: string) {
+        if (this.constructed) {
+            this.firePlayerEvent('toast', { content: html });
+        }
+    }
+    seek(time: number) {
+        const fixedTime = clamp(time, 0, this.video.duration);
+        this.toast(
+            `Seek: ${fTime(fixedTime, this.overHour)} / ${fTime(
+                this.video.duration
+            )}`
+        );
+        this.video.currentTime = fixedTime;
+    }
+    seekPercent(percent: number) {
+        this.seek(this.video.duration * percent);
+    }
+    skip(time: number) {
+        this.seek(this.video.currentTime + time);
+    }
+    play() {
+        this.video.play().catch((e) => alert(e));
+        this.toast('Play');
+    }
+    pause() {
+        this.video.pause();
+        this.toast('Pause');
+    }
+    togglePlay() {
+        if (this.video.paused) {
+            this.play();
+        } else {
+            this.pause();
+        }
+    }
+    mute() {
+        this.video.muted = true;
+        this.setContainerData('muted', true);
+        this.firePlayerEvent('mute');
+        this.toast('Mute');
+    }
+    unmute() {
+        this.video.muted = false;
+        this.setContainerData('muted', false);
+        this.firePlayerEvent('unmute');
+        this.toast('Unmute');
+    }
+    toggleMute() {
+        if (this.video.muted) {
+            this.unmute();
+        } else {
+            this.mute();
+        }
+    }
+    requestFullscreen() {
+        this.container.requestFullscreen();
+        this.focus();
+    }
+    exitFullscreen() {
+        document.exitFullscreen().catch(() => {});
+        this.focus();
+    }
+    toggleFullscreen() {
+        if (this.getContainerData('fullscreen') === 'true') {
+            this.exitFullscreen();
+        } else {
+            this.requestFullscreen();
+        }
+    }
+    setVolume(volume: number) {
+        const fixedVolume = clamp(volume, 0, 1);
+        this.video.volume = fixedVolume;
+        this.toast(`Volume: ${Math.round(fixedVolume * 100)}%`);
+    }
+    adjustVolume(volume: number) {
+        if (!this.video.muted) {
+            this.setVolume(this.video.volume + volume);
+        }
+    }
+}
---
+++ src/player.metadata.ts
@@ -0,0 +1,139 @@
+type MetaEventTypes = 'selfEvent' | 'playerEvent' | 'videoEvent';
+
+type MetaEvents<F extends AnyFunction> =
+    | EventListenerMap<F>
+    | (() => EventListenerMap<F>);
+
+type ElementMetaEvents<T extends HTMLTagNames> = MetaEvents<
+    (player: Player, element: HTMLElementTagNameMap[T]) => void
+>;
+type ElementVideoMetaEvents<T extends HTMLTagNames> = MetaEvents<
+    (
+        player: Player,
+        element: HTMLElementTagNameMap[T],
+        video: HTMLVideoElement
+    ) => void
+>;
+
+function bindMetadataEvents( // TODO improve
+    data: { [s in MetaEventTypes]?: MetaEvents<any> },
+    settings: { [s in MetaEventTypes]?: [HTMLElement, () => any] }
+) {
+    for (const [name, target_params] of Object.entries(settings)) {
+        if (data[name as MetaEventTypes]) {
+            bindEvent(
+                target_params[0],
+                ternaryWithCallback(
+                    data[name as MetaEventTypes] as MetaEvents<any>,
+                    (v) => typeof v === 'function',
+                    (v) => v(),
+                    (v) => v
+                ),
+                target_params[1]()
+            );
+        }
+    }
+}
+
+class EDC<T extends HTMLTagNames> {
+    private tag: T;
+    private name: string;
+    private _condition: (player: Player) => boolean;
+    private _css: (data: EDC<T>) => string;
+    attributes: StrKV = {};
+    private _html: string;
+    private _selfEvent: ElementMetaEvents<T>;
+    private _playerEvent: ElementMetaEvents<T>;
+    private _videoEvent: ElementVideoMetaEvents<T>;
+    private _childrenBuilders: EDC<any>[] = [];
+    private _children: HTMLElement[] = [];
+    constructor(tag: T, name?: string) {
+        this.tag = tag;
+        this.name = name;
+    }
+    attr(attributes: StrKV) {
+        this.attributes = attributes;
+        return this;
+    }
+    children(...c: (EDC<any> | HTMLElement)[]) {
+        for (const e of c) {
+            if (e instanceof HTMLElement) {
+                this._children.push(e);
+            } else {
+                this._childrenBuilders.push(e);
+            }
+        }
+        return this;
+    }
+    condition(f: (player: Player) => boolean) {
+        this._condition = f;
+        return this;
+    }
+    css(sup: (data: EDC<T>) => string) {
+        this._css = sup;
+        return this;
+    }
+    html(html: string) {
+        this._html = html;
+        return this;
+    }
+    selfEvents(map: ElementMetaEvents<T>) {
+        this._selfEvent = map;
+        return this;
+    }
+    playerEvents(map: ElementMetaEvents<T>) {
+        this._playerEvent = map;
+        return this;
+    }
+    videoEvents(map: ElementVideoMetaEvents<T>) {
+        this._videoEvent = map;
+        return this;
+    }
+    create(player: Player): HTMLElementTagNameMap[T] | null {
+        if (this._condition && !this._condition(player)) {
+            return null;
+        }
+        const element = document.createElement(this.tag);
+        for (const [key, value] of Object.entries(this.attributes)) {
+            element.setAttribute(key, value);
+        }
+        if (this._html) {
+            element.innerHTML = this._html;
+        }
+        if (this._css) {
+            player.style.textContent += this._css(this);
+        }
+        bindMetadataEvents(
+            {
+                selfEvent: this._selfEvent,
+                playerEvent: this._playerEvent,
+                videoEvent: this._videoEvent,
+            },
+            {
+                selfEvent: [element, () => [player, element]],
+                playerEvent: [player.container, () => [player, element]],
+                videoEvent: [
+                    player.video,
+                    () => [player, element, player.video],
+                ],
+            }
+        );
+        for (const childBulder of this._childrenBuilders) {
+            appendChild(element, childBulder.create(player));
+        }
+        for (const childElement of this._children) {
+            appendChild(element, childElement);
+        }
+        if (this.name) {
+            player.elements[this.name] = element;
+        }
+        element.dispatchEvent(new CustomEvent('create'));
+        return element;
+    }
+}
+
+interface PlayerMetadata {
+    elements?: EDC<any>[];
+    playerEvent?: MetaEvents<(player: Player) => void>;
+    videoEvent?: MetaEvents<(player: Player, video: HTMLVideoElement) => void>;
+}
