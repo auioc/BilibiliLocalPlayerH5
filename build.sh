e56d5121dbe7cbe19ef540c919acdfefece1fa68
WakelessSloth56 <wsloth56@foxmail.com>
2024-03-12 16:46:54 +0800

    build: fix aio ccl style

--- build.sh
+++ build.sh
@@ -27,11 +27,6 @@ terser --config-file temp/terser.json public/player.js >temp/player.js
 html-minifier-terser --collapse-whitespace public/player.css >temp/player.css
 # Download CCL style
 curl -L https://github.com/jabbany/CommentCoreLibrary/raw/19db2962ed0ce637a2b99facdf8634d51bb1b503/dist/css/style.min.css >temp/ccl.css
-# Build all.css
-cat public/player.css >temp/all.css
-cat temp/ccl.css >>temp/all.css
-# Minify all.css
-html-minifier-terser --collapse-whitespace temp/player.css >temp/all.css
 
 # Minify index.html
 html-minifier-terser --minify-js --collapse-whitespace public/index.html >temp/index.html
@@ -39,8 +34,13 @@ html-minifier-terser --minify-js --collapse-whitespace public/index.html >temp/i
 # Build AIO html
 cp temp/index.html temp/aio.html
 sed -i 's;<link[^>]*>;;g' temp/aio.html
-sed -i 's^\\^\\\\^g' temp/all.css
-sed -i "s^<\!--allcss-->^<style>$(cat temp/all.css)</style>^g" temp/aio.html
+## Build all.min.css and fill into html
+cat public/player.css >temp/all.css
+cat temp/ccl.css >>temp/all.css
+html-minifier-terser --collapse-whitespace temp/all.css >temp/all.min.css
+sed -i 's^\\^\\\\^g' temp/all.min.css
+sed -i "s^<\!--allcss-->^<style>$(cat temp/all.min.css)</style>^g" temp/aio.html
+## Patch and fill script into html
 sed -i 's;<script src="player.js"></script>;;g' temp/aio.html
 sed 's^\\^\\\\^g' temp/player.js >temp/player.pathced.js
 sed -i 's^\&^\\&^g' temp/player.pathced.js
