9f6410d669cc0b3e4936370df2a3c297f203c5f8
WakelessSloth56 <wsloth56@foxmail.com>
2024-03-15 09:18:27 +0800

    chore: copyright notice

--- src/player.main.ts
+++ src/player.main.ts
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2022-2024 AUIOC.ORG
+ * Copyright (C) 2018-2022 PCC-Studio
+ *
+ * This file is part of BilibiliLocalPlayerH5.
+ *
+ * BilibiliLocalPlayerH5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 interface PlayerOptions extends StrAnyKV {
     autoPlay?: boolean;
     muted?: boolean;
@@ -5,6 +25,11 @@ interface PlayerOptions extends StrAnyKV {
 }
 
 class Player {
+    /**
+     * This field will be automatically replaced to
+     * current version text during the CI build.
+     * Do not modify it!
+     **/
     readonly version = '{version}';
     options: PlayerOptions;
     readonly #metadata: PlayerMetadata;
