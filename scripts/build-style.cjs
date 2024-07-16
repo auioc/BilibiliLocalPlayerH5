cb9ce33efe23d8ac64861779f7cf9df27769a246
WakelessSloth56 <wsloth56@foxmail.com>
2024-07-16 19:19:21 +0800

    build: remove styles dev-build
    [skip ci]

--- scripts/build-style.cjs
+++ scripts/build-style.cjs
@@ -1,12 +1,9 @@
 const { DEV, inputs, outputs } = require('./const.cjs');
-const fs = require('fs');
 const { minify } = require('html-minifier-terser');
 const { html: terserOptions } = require('./terser.config.cjs');
 const { readFile, writeFile } = require('./utils.cjs');
 
-if (DEV) {
-    fs.copyFileSync(inputs.style, outputs.style.dev);
-} else {
+if (!DEV) {
     const css = readFile(inputs.style);
     (async () => {
         const minCss = await minify(css, terserOptions);
