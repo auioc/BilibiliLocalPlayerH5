e5ed66a748659e286abc7316640cd5593e07dddd
WakelessSloth56 <wsloth56@foxmail.com>
2024-08-28 20:11:50 +0800

    build: license banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
+const license = require('rollup-plugin-license');
 const { version, srcPath } = require('./scripts/utils.cjs');
 const { javascript: terserOptions } = require('./scripts/terser.config.cjs');
 
 const dev = process.env.NODE_ENV !== 'production';
+const ver = version();
 
 const ccl = '../lib/CommentCoreLibrary.js';
 
@@ -32,8 +34,21 @@ export default [
                 // (!) [plugin replace] @rollup/plugin-replace: 'preventAssignment' currently defaults to false. It is recommended to set this option to `true`, as the next major version will default this option to `true`.
                 preventAssignment: true,
                 values: {
-                    // _version_: `JSON.parse('${JSON.stringify(version())}');`,
-                    _version_: JSON.stringify(version()) + ';',
+                    // _version_: `JSON.parse('${JSON.stringify(ver)}');`,
+                    _version_: JSON.stringify(ver) + ';',
+                },
+            }),
+            license({
+                banner: {
+                    commentStyle: 'ignored',
+                    content: `
+Bundle of BilibiliLocalPlayerH5 (https://github.com/auioc/BilibiliLocalPlayerH5)
+Generated at <%= moment().format() %>
+Version: v<%= pkg.version %> @ ${ver.text}
+Copyright (C) 2022-<%= moment().format('YYYY') %> AUIOC.ORG
+Copyright (C) 2018-2022 PCC-Studio
+Licensed under GNU Affero General Public License v3.0 (https://github.com/auioc/BilibiliLocalPlayerH5/blob/main/LICENSE)
+`.trim(),
                 },
             }),
             ...[dev ? [] : [terser(terserOptions)]],
